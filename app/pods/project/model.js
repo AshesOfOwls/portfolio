8513e46a04163d405c1722c5fb73c440c2a6c4d2
Scott Elwood <mechpants@gmail.com>
2016-04-13 00:21:46 -0700

    Getting parameters correctly parsed that are camel cased, more styles, better things

--- app/pods/project/model.js
+++ app/pods/project/model.js
@@ -2,7 +2,9 @@ import Model from 'ember-data/model';
 import DS from 'ember-data';
 
 export default Model.extend({
-  name: DS.attr('string'),
   type: DS.attr('string'),
-  description: DS.attr('string')
+  name: DS.attr('string'),
+  description: DS.attr('string'),
+  repoLink: DS.attr('string'),
+  demoLink: DS.attr('string')
 });
